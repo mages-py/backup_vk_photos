b6da47cfbaa172775530cc2fee430f6ddc239385
Evgeniy.Gorbunov <Evgeniy.Gorbunov@polaru.com>
2024-02-15 23:58:00 +0300

    Reorderd code

--- app.py
+++ app.py
@@ -1,38 +1,42 @@
 from vk_client import VKClient
 from pprint import pprint
 
-def start(user_id):
-    client = VKClient(user_id=user_id)
-    albums = client.get_albums()
+def get_album_id(client_vk):
+    albums = client_vk.get_albums()
     if 'error' in albums:
         print('Текст ошибки:', albums['error']['error_msg'])
         return
+    albums_arr = [{'profile':'Фото профиля'}, {'wall': 'Фото со стены'}]
+    albums_arr += [{album['id']: album['title']} for album in albums['response']['items'] if int(album['id']) > 0]
     print('Список альбомов:')
     print('===================================================================')
-    for album in albums['response']['items']:
-        print(album['id'], album['title'])
+    for idx, album in enumerate(albums_arr, start=1):
+        print(idx, list(album.values())[0])
     print('===================================================================')
-    album_id = int(input('Введите ID альбома: '))
-    if album_id == 0:
+    album_id = int(input('Введите номер альбома: '))
+    if album_id <= 0 or album_id > len(albums_arr):
+        print('Нет такого альбома')
         return
-    photos = client.get_photos(album_id)
+    return list(albums_arr[album_id-1].keys())[0]
+
+def get_sorted_photos(vk_client, album_id):
+    photos = vk_client.get_photos(album_id)
     if 'error' in photos:
         print('Текст ошибки:', photos['error']['error_msg'])
         return
-    print('Список фотографий:')
-    print('===================================================================')
-    save_photos = []
-    for photo in photos['response']['items']:
-        if 'likes' in photo:
-            # print(photo['likes']['count'], photo['id'], photo['sizes'][-1]['url'], photo['date'])
-            save_photos.append(
-                {
-                    'likes': photo['likes']['count'],
-                    'date': photo['date'],
-                    'url': photo['sizes'][-1]['url']
-                }
-            )
-    print(save_photos)
+    return sorted(photos['response']['items'], key=lambda photo: photo['likes']['count'], reverse=True)
+    
+
+def start(user_id, qty_photos=5):
+    client_vk = VKClient(user_id=user_id)
+    
+    album_id = get_album_id(client_vk)
+    if not album_id:
+        print('Не выбран альбом')
+        return
+    
+    photos = get_sorted_photos(client_vk, album_id)
+    pprint(photos[0:qty_photos])
     # print('===================================================================')
     # photo_id = int(input('Введите ID фотографии: '))
     # if photo_id < 0:
